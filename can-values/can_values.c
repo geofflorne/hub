9b8386ae5da3c36a1e3eb0c2b42eacfd890aad0f
Sean Burt <seandburt@gmail.com>
2017-01-26 14:42:45 -0800

    Can values and read can Update
    update can-values template file
    add read can example c file

--- can-values/can_values.c
+++ can-values/can_values.c
@@ -1,166 +1,160 @@
-// Includes
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
 
-// Prototypes
-void can_signal_create(char* signal, char* name, char* unit);
-void can_signal_set_value(char* signal, int value);
-int CAN_PARSE_UINT16(uint16_t can_data, int init_val); // returns decimal int
-
-
-typedef struct Signals {
-	char* signal;
-	char* name;
-	char* unit;
-} Signals;
-
-// Main!
 int main() {
 
-	// Frame 1 (0x600)
-	can_signal_create("rpm", "RPM", "rpm");
-	can_signal_create("vbat", "Battery Voltage", "V");
-
-	// Frame 2 (0x601)
-	// Frame 3 (0x602)
-
-	// Frame 4 (0x603)
-	can_signal_create("lam1", "Fuel Air Ratio (Lambda)", "lambda");
-
-	// Frame 5 (0x604)
-
-	// Frame 6 (0x605)
-	can_signal_create("ect1", "Engine Coolant Temperature", "degC");
-	can_signal_create("egt1", "Exhaust Gas Temperature", "degC");
-
-	// Frame 7 (0x606)
-	// Frame 8 (0x607)
-	// Frame 9 (0x608)
-
-	// Frame 10 (0x609)
-	can_signal_create("eot", "Engine Oil Temperature", "degC");
-
-	// Frame 11 (0x610)
+	
 	can_signal_create("calSwitch", "Calibration Switch Position", "None");
-	can_signal_create("tcSwitch", "Traction Control Switch Position", "None");
-
-	// Frame 12 (0x611)
-	// Frame 13 (0x612)
-	// Frame 14 (0x613)
-
-	// Frame 15 (0x614)
-	can_signal_create("gear", "Gear Location", "");
-
-	// Frame 16 (0x615)
-	// Frame 17 (0x616)
-	// Frame 18 (0x617)
-
-	// unknown
-	can_signal_create("mat", "Manifold Temperature", "degC");
+	
 	can_signal_create("rm1", "Run Mode", "");
-
-	// test can_id
-	int can_id = 600;
-	int value = 10;
-	uint16_t can_data = 141; //0x8D
+	
+	can_signal_create("mat", "Manifold Temperature", "degC");
+	
+	can_signal_create("lam1", "Fuel Air Ratio (Lambda)", "lambda");
+	
+	can_signal_create("egt1", "Exhaust Gas Temperature", "degC");
+	
+	can_signal_create("vbat", "Battery Voltage", "V");
+	
+	can_signal_create("gear", "Gear Location", "");
+	
+	can_signal_create("eot", "Engine Oil Temperature", "degC");
+	
+	can_signal_create("tcSwitch", "Traction Control Switch Position", "None");
+	
+	can_signal_create("rpm", "RPM", "rpm");
+	
+	can_signal_create("ect1", "Engine Coolant Temperature", "degC");
+	
 
 	switch (can_id) {
-
-		case 0x600: // Frame 1
-
+	
+	case 0x614:
+		
+		
 			value = CAN_PARSE_UINT16(can_data, 0);
+			
 
-			can_signal_set_value("rpm", value);
+			can_signal_set_value("gear", value);
+			
+		
+		break;
+	
+	case 0x615:
+		
+		
 			value = CAN_PARSE_UINT16(can_data, 0);
-				value = value / 1000;
-
-
-			can_signal_set_value("vbat", value);
-			value = CAN_PARSE_UINT16(can_data, 0);
+			
 				value = value / 10;
+			
 
-			break;
-
-		case 0x603: // Frame 4
-
-			can_signal_set_value("lam1", value);
+			can_signal_set_value("mat", value);
+			
+		
 			value = CAN_PARSE_UINT16(can_data, 0);
-				value = value / 10;
+			
 
-			break;
-
-		case 0x605: // Frame 6
-
-			can_signal_set_value("ect1", value);
+			can_signal_set_value("rm1", value);
+			
+		
+		break;
+	
+	case 0x610:
+		
+		
 			value = CAN_PARSE_UINT16(can_data, 0);
-				value = value / 10;
-
-			can_signal_set_value("egt1", value);
-				value = CAN_PARSE_UINT16(can_data, 0);
-
-			break;
-
-		case 0x609: // Frame 10
-
-			can_signal_set_value("eot", value);
-			value = CAN_PARSE_UINT16(can_data, 0);
-				value = value / 10;
-
-			break;
-
-		case 0x610: // Frame 11
+			
+				value = value + 1;
+			
 
 			can_signal_set_value("calSwitch", value);
+			
+		
 			value = CAN_PARSE_UINT16(can_data, 0);
+			
 				value = value + 1;
-
+			
 
 			can_signal_set_value("tcSwitch", value);
+			
+		
+		break;
+	
+	case 0x609:
+		
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
+				value = value / 10;
+			
 
-			break;
+			can_signal_set_value("eot", value);
+			
+		
+		break;
+	
+	case 0x603:
+		
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
+				value = value / 1000;
+			
 
-		case 0x14:
+			can_signal_set_value("lam1", value);
+			
+		
+		break;
+	
+	case 0x600:
+		
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
 
-				can_signal_set_value("gear", value);
-				value = CAN_PARSE_UINT16(can_data, 0);
-					value = value + 1;
+			can_signal_set_value("rpm", value);
+			
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
+				value = value / 1000;
+			
 
-	 		break;
+			can_signal_set_value("vbat", value);
+			
+		
+		break;
+	
+	case 0x605:
+		
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
+				value = value / 10;
+			
 
-		/*
+			can_signal_set_value("ect1", value);
+			
+		
+			value = CAN_PARSE_UINT16(can_data, 0);
+			
+				value = value / 10;
+			
 
-
-					can_signal_set_value("mat", value);
-					value = CAN_PARSE_UINT16(can_data, 0);
-						value = value / 1000;
-
-
-				can_signal_set_value("rm1", value);
-				value = CAN_PARSE_UINT16(can_data, 0);
-
-		*/
+			can_signal_set_value("egt1", value);
+			
+		
+		break;
+	
 	}
-}
 
-// Functions
+	void can_signal_create(char* signal, char* name, char* unit) {
 
-// Create the signal and set its parameters
-void can_signal_create(char* signal, char* name, char* unit) {
+	}
 
-	Signals new_signal;
+	void can_signal_set_value(char* signal, int value) {
 
-	strcpy(new_signal.signal, signal);
+	}
 
-}
+	int CAN_PARSE_UINT16(uint16_t can_data, int init_val) {
 
-// set the vaue to the signal
-void can_signal_set_value(char* signal, int value) {
-
-}
-
-int CAN_PARSE_UINT16(uint16_t can_data, int init_val) {
-
-	return 0;
-
+	}
 }
---
+++ can-values/can_values.h
@@ -0,0 +1,20 @@
+// Functions
+// Create the signal and set its parameters
+void can_signal_create(char* signal, char* name, char* unit) {
+
+	Signals new_signal;
+
+	strcpy(new_signal.signal, signal);
+
+}
+
+// set the vaue to the signal
+void can_signal_set_value(char* signal, int value) {
+
+}
+
+int CAN_PARSE_UINT16(uint16_t can_data, int init_val) {
+
+	return 0;
+
+}
